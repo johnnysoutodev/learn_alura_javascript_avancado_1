317ce90f4ef36714170432434b4205588e85e4c4
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2020-10-27 22:14:03 -0300

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,13 @@
+// index.js
+
+const cliente1Nome = "Ricardo";
+const cliente1CPF = 11122233309;
+const cliente1Agencia = 1001;
+const client1Saldo = 0;
+
+const cliente2Nome = "Alice";
+const cliente2CPF = 88822233309;
+const cliente2Agencia = 1001;
+const cliente2Saldo = 0;
+
+console.log(cliente1Nome);
