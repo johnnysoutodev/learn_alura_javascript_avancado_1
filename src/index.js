b4ebc1dd3490f774a1270c8d3c4798ea0926e3f7
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2020-10-28 00:27:18 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,24 +1,5 @@
 // index.js
 
-class ContaCorrente{
-    agencia;
-    _saldo = 0;
-
-    sacar(valor){
-        if(this._saldo >= valor){
-            this._saldo -= valor;
-            return valor;
-        }
-    }
-
-    depositar(valor){
-        if(valor <= 0){
-            return;
-        }
-        this._saldo += valor;
-    }
-}
-
 const cliente1 = new Cliente();
 cliente1.nome = "Ricardo";
 cliente1.cpf = 11122233309;
