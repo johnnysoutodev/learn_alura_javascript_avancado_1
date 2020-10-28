a175d6ba2d88a02ed32db9870639af2f5b4cf2fe
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2020-10-27 23:03:10 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,24 +3,49 @@
 class Cliente{
     nome;
     cpf;
-    agencia;
-    saldo;
     rg;
 }
 
-const cliente1 = new Cliente();
-const cliente2 = new Cliente();
+class ContaCorrente{
+    agencia;
+    saldo;
 
+    sacar(valor){
+        if(this.saldo >= valor){
+            this.saldo -= valor;
+        }
+    }
+
+    depositar(valor){
+        if(valor > 0){
+            this.saldo += valor;
+        }
+    }
+}
+
+const cliente1 = new Cliente();
 cliente1.nome = "Ricardo";
 cliente1.cpf = 11122233309;
 cliente1.agencia = 1001;
 cliente1.saldo = 0;
 cliente1.rg = 123456789;
 
+const cliente2 = new Cliente();
 cliente2.nome = "Alice";
 cliente2.cpf = 88822233309;
 cliente2.agencia = 1001;
 cliente2.saldo = 0;
 
+const contaCorrenteRicardo = new ContaCorrente();
+contaCorrenteRicardo.saldo = 1000;
+contaCorrenteRicardo.agendcia = 1001;
+
+const contaCorrenteAlice = new ContaCorrente();
+contaCorrenteAlice.saldo = 0;
+contaCorrenteAlice.agencia = 1001;
+
+contaCorrenteRicardo.sacar(200);
+
 console.log(cliente1);
 console.log(cliente2);
+console.log(contaCorrenteRicardo.saldo);
