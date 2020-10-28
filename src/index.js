53e2f98c91b9230abdfae576f10909a1980b823c
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2020-10-28 02:24:32 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,25 +6,27 @@ import {ContaCorrente} from "./ContaCorrente.js"
 const cliente1 = new Cliente();
 cliente1.nome = "Ricardo";
 cliente1.cpf = 11122233309;
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
 cliente1.rg = 123456789;
 
 const cliente2 = new Cliente();
 cliente2.nome = "Alice";
 cliente2.cpf = 88822233309;
-cliente2.agencia = 1001;
-cliente2.saldo = 0;
+cliente2.rg = 123456780;
 
 const contaCorrenteRicardo = new ContaCorrente();
 // contaCorrenteRicardo.#saldo = 0;
 contaCorrenteRicardo.agencia = 1001;
+contaCorrenteRicardo.cliente = cliente1;
 
 const contaCorrenteAlice = new ContaCorrente();
 // contaCorrenteAlice.#saldo = 0;
 contaCorrenteAlice.agencia = 1001;
+contaCorrenteAlice.cliente = cliente2;
 
 contaCorrenteRicardo.depositar(1000);
 contaCorrenteRicardo.sacar(200);
 
+
+
 console.log(contaCorrenteRicardo);
+console.log(contaCorrenteAlice);
